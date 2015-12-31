40db5ad9e98e0ad58af12e1b6fb2e6ec4fe1f7cc
pupnewfster <richard@freimer.com>
2015-12-30 22:32:40 -0500

    Increase number of ranks it can sort on tablist

--- Necessities/src/com/crossge/necessities/ScoreBoards.java
+++ Necessities/src/com/crossge/necessities/ScoreBoards.java
@@ -9,6 +9,7 @@ import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
 
 public class ScoreBoards {
+    private static String[] ALPHABET = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     private static ScoreboardManager man;
     private static Scoreboard b;
     RankManager rm = new RankManager();
@@ -17,8 +18,8 @@ public class ScoreBoards {
         man = Bukkit.getScoreboardManager();
         b = man.getMainScoreboard();//Use main scoreboard instead of a new one for better compatability with other plugins
         for (Rank r : rm.getOrder())
-            if (b.getTeam((rm.getOrder().size() - rm.getOrder().indexOf(r)) + "") == null) {
-                Team t = b.registerNewTeam((rm.getOrder().size() - rm.getOrder().indexOf(r)) + "");
+            if (b.getTeam(fromInt(rm.getOrder().size() - rm.getOrder().indexOf(r))) == null) {
+                Team t = b.registerNewTeam(fromInt(rm.getOrder().size() - rm.getOrder().indexOf(r)));
                 t.setPrefix(r.getColor());
             }
     }
@@ -26,7 +27,7 @@ public class ScoreBoards {
     public void addPlayer(User u) {
         if (u.getRank() == null || u.getPlayer() == null)
             return;
-        Team t = b.getTeam((rm.getOrder().size() - rm.getOrder().indexOf(u.getRank())) + "");
+        Team t = b.getTeam(fromInt(rm.getOrder().size() - rm.getOrder().indexOf(u.getRank())));
         if (t == null)
             return;
         for (Team team : b.getTeams())
@@ -41,10 +42,15 @@ public class ScoreBoards {
             return;
         Team t = b.getTeam(u.getPlayer().getName());
         if (t == null)
-            t = b.getTeam((rm.getOrder().size() - rm.getOrder().indexOf(u.getRank())) + "");
+            t = b.getTeam(fromInt(rm.getOrder().size() - rm.getOrder().indexOf(u.getRank())));
         if (t == null)
             return;
         t.removeEntry(u.getPlayer().getName());
         u.getPlayer().setScoreboard(man.getNewScoreboard());
+    }
+
+    private String fromInt(int toTranslate) {
+        toTranslate -= 1;
+        return (toTranslate < 0 || ALPHABET.length <= toTranslate) ? "ERROR" : ALPHABET[toTranslate];
     }
 }
