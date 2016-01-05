af69153e2d5a68fc7a78dfdf7ceef1ade1d9a946
Eddie Penta <ecp4224@gmail.com>
2016-01-04 20:49:32 -0500

    Buffed rewards slightly

--- lavasurvival/src/main/java/me/eddiep/minecraft/ls/game/Gamemode.java
+++ lavasurvival/src/main/java/me/eddiep/minecraft/ls/game/Gamemode.java
@@ -677,10 +677,10 @@ public abstract class Gamemode {
 
         double base = 100.0;
         Rank rank = Lavasurvival.INSTANCE.getNecessitiesUserManager().getUser(player.getUniqueId()).getRank();
-        double bonusAdd = (1 + Lavasurvival.INSTANCE.getRankManager().getOrder().indexOf(rank)) / 2.0;
+        double bonusAdd = (5 + Lavasurvival.INSTANCE.getRankManager().getOrder().indexOf(rank)) / 2.0;
 
         //int blockCount = countAirBlocksAround(onlinePlayer, 20);
-        System.out.println(onlinePlayer.getName() + " had " + blockCount + " blocks around them!");
+        //System.out.println(onlinePlayer.getName() + " had " + blockCount + " blocks around them!");
 
         return base + (bonusAdd * blockCount);
     }
