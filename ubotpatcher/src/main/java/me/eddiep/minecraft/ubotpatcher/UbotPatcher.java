558303bfed20087482ea58d87a7e02ec8d86eed7
pupnewfster <richard@freimer.com>
2016-11-07 18:28:45 -0500

    Update do using lambda expression

--- ubotpatcher/src/main/java/me/eddiep/minecraft/ubotpatcher/UbotPatcher.java
+++ ubotpatcher/src/main/java/me/eddiep/minecraft/ubotpatcher/UbotPatcher.java
@@ -3,24 +3,16 @@ package me.eddiep.minecraft.ubotpatcher;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
 public class UbotPatcher extends JavaPlugin {
-
     @Override
     public void onEnable() {
         getLogger().info("Searching for jar files...");
 
-        File[] jars = getDataFolder().listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.getName().endsWith("jar");
-            }
-        });
+        File[] jars = getDataFolder().listFiles(pathname -> pathname.getName().endsWith("jar"));
 
         if (jars == null) {
             getLogger().info("No jar files found!");
@@ -33,7 +25,6 @@ public class UbotPatcher extends JavaPlugin {
         for (File f : jars) {
             getLogger().info("Moving " + f.getName() + "...");
             File target = new File(getDataFolder().getParentFile(), f.getName());
-
             try {
                 Files.move(f.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
