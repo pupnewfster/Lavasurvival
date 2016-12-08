03128d33f57e68ce4ad66609d06afc3e46c9f969
pupnewfster <richard@freimer.com>
2016-12-08 18:11:59 -0500

    Update patch file to use correct getDataFolder() of UBot. Also made it so that old world files could be deleted so the new ones could be created

--- patch.sh
+++ patch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo Patching Lavasurvival...
-cp /home/minecraft/ubot/Lavasurvival/lavasurvival/target/lavasurvival-0.3.jar /home/minecraft/lavasurvival/plugins/ubotpatcher/Lavasurvival.jar
+cp /home/minecraft/ubot/Lavasurvival/lavasurvival/target/lavasurvival-0.3.jar /home/minecraft/lavasurvival/plugins/UBotPatcher/Lavasurvival.jar
 
 echo Patching ClassicPhysics...
-cp /home/minecraft/ubot/Lavasurvival/classicphysics/target/classicphysics-1.0.jar /home/minecraft/lavasurvival/plugins/ubotpatcher/ClassicPhysics.jar
+cp /home/minecraft/ubot/Lavasurvival/classicphysics/target/classicphysics-1.0.jar /home/minecraft/lavasurvival/plugins/UBotPatcher/ClassicPhysics.jar
 
 echo Patching Necessities...
-cp /home/minecraft/ubot/Lavasurvival/necessities/target/necessities-1.0.jar /home/minecraft/lavasurvival/plugins/ubotpatcher/Necessities.jar
+cp /home/minecraft/ubot/Lavasurvival/necessities/target/necessities-1.0.jar /home/minecraft/lavasurvival/plugins/UBotPatcher/Necessities.jar
 
 echo Done!
